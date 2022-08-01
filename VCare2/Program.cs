ebe57f87b04700030d9befb60485d5c098a19b24
Simon Entwisle <bibiplone@hotmail.com>
2022-08-01 21:07:49 +0100

    All layout and functionality done.

--- VCare2/Program.cs
+++ VCare2/Program.cs
@@ -50,7 +50,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=staffs}/{action=Index}/{id?}");
 
 app.Run();
 
